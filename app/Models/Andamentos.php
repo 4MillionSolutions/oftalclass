5b621ee04400714ed8bfeee0c96c7f6387b2f5a1
douglasdacosta <you@example.com>
2025-05-24 15:45:37 -0300

    first commit - front end

---
+++ app/Models/Andamentos.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Andamentos extends Model
+{
+    use HasFactory;
+}
---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+
+$(function ($) {
+    const myModal = document.getElementById('exampleModal')
+    const myInput = document.getElementById('myInput')
+
+    myModal.addEventListener('shown.bs.modal', () => {
+    myInput.focus()
+    })
+
+})
