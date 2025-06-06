fb27c66e72e5d76ca146cfef33299653b2ad9d79
douglasdacosta <doug.d.costa@gmail.com>
2025-06-06 15:58:07 -0300

    Telas de indicações

--- public/js/main_custom.js
+++ public/js/main_custom.js
@@ -172,7 +172,7 @@ function compartilharLink() {
     const codigo = document.getElementById('codigo_indicacao').value;
 
     const texto = encodeURIComponent(
-        `Ganhe desconto na OftalClass e ajude um amigo a receber cashback!\n\n` +
+        `Ganhe desconto na OftalClass!\n\n` +
         `Você recebeu um link com benefício exclusivo!\n\n` +
         `Use o código *${codigo}* na hora da compra e ganhe desconto em seus produtos.\n\n` +
         `Cadastre-se agora e aproveite:\n` +
